77205efca39110f3939753f87fbe78090d5a6b10
userName <userEmail>
2024-09-30 22:53:33 -0500

    Add assets and modify login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,49 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
 const Login = () => {
-    return <div>this is login</div>
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form validateTrigger="onBlur">
+          <Form.Item 
+            name="mobile"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your mobile number!',
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '手机号码格式不对'
+              }
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your code!',
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
