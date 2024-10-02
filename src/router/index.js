8444bf24cad5df6438aaee33a34086b37ea12b0a
userName <userEmail>
2024-10-02 00:17:41 -0500

    二级路由的配置

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,36 @@
 
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
+import Publish from '@/pages/Publish';
+import Article from '@/pages/Article';
+import Home from '@/pages/Home';
 
 import { createBrowserRouter } from 'react-router-dom'
 import { AuthRoute } from '@/components/AuthRoute'
 
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <AuthRoute> <Layout /></AuthRoute>
-    },
-    {
-      path: '/login',
-      element: <Login />
-    },
-  ])
+  {
+    path: '/',
+    element: <AuthRoute> <Layout /></AuthRoute>,
+    children: [
+      {
+        path: 'home',
+        element: <Home />
+      },
+      {
+        path: 'article',
+        element: <Article />
+      },
+      {
+        path: 'publish',
+        element: <Publish />
+      }
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+])
   
   export default router
