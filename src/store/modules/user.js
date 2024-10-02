4ed4abea3e34d0e8629b0d304b8f2a090e8d7d0a
userName <userEmail>
2024-10-02 15:40:59 -0500

    layout-获取渲染个人信息

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -8,19 +8,23 @@ const userStore = createSlice({
   name: 'user',
   // 数据状态
   initialState: {
-    token: getToken() || ''
+    token: getToken() || '',
+    userInfo: {}
   },
   // 同步修改方法
   reducers: {
     setToken (state, action) {
       state.token = action.payload
       _setToken(action.payload)
+    },
+    setUserInfo (state, action) {
+      state.userInfo = action.payload
     }
   }
 })
 
 // 解构出actionCreater
-const { setToken } = userStore.actions
+const { setToken, setUserInfo } = userStore.actions
 
 // 获取reducer函数
 const userReducer = userStore.reducer
@@ -36,6 +40,14 @@ const fetchLogin = (loginForm) => {
   }
 }
 
-export { fetchLogin, setToken }
+// 异步方法获取用户信息
+const fetchUserInfo = () => {
+  return async (dispatch) => {
+    const res = await request.get('/user/profile')
+    dispatch(setUserInfo(res.data))
+  }
+}
+
+export { fetchLogin, fetchUserInfo, setToken }
 
 export default userReducer
