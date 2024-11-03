73407ddfa713ee4b216618f3674ace1a4028ff7f
userName <userEmail>
2024-11-02 23:35:27 -0500

    文章列表-基础table渲染

--- src/apis/article.js
+++ src/apis/article.js
@@ -18,3 +18,14 @@ export function createArticleAPI(data) {
     data
   })
 }
+
+
+// 获取文章列表
+
+export function getArticleListAPI(params) {
+  return request({
+    url: "/mp/articles",
+    method: 'GET',
+    params
+  })
+}
